1850009aa24d656cd77d457cdd69d7af9cd9d2ae
hagoromo2000 <revtm000126@gmail.com>
2023-02-17 17:10:15 +0900

    Add:tailwindとdaisyuiの導入 #2 #4

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/*.{js,ts,jsx,tsx}",
+    "./index.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
