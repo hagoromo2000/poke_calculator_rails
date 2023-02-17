1fae4ef38c9e6bfbb2c4f4223d5f72dcbd62d50f
hagoromo2000 <revtm000126@gmail.com>
2023-02-17 19:37:16 +0900

    Add:Headerの雛形の作成 #7

---
+++ frontend/src/components/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <>
+      <div className="artboard phone-3 bg-white rounded-lg shadow-xl mx-auto pt-300">414×736</div>
+    </>
+  )
+}
+
+export default Calculator
