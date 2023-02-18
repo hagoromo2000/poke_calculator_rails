a7f41bc4f7aaea59dcf408796491b73d4aac1cc9
hagoromo2000 <revtm000126@gmail.com>
2023-02-18 22:54:45 +0900

    投稿一覧画面の雛形を作成 #11

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Calculator from './components/Calculator';
 import Header from './components/Header';
+import IndexPosts from "./components/IndexPosts";
 import NewPost from "./components/NewPost";
 
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-green-200 to-blue-200 ... ">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route exact path={`/`} element={<Calculator />} />
-          <Route path={`/posts/new/`} element={<NewPost />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="min-h-screen bg-gradient-to-r from-green-200 to-blue-200 ... ">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route exact path={`/`} element={<Calculator />} />
+            <Route exact path={`/posts/new/`} element={<NewPost />} />
+            <Route exact path={`/posts/`} element={<IndexPosts/>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/IndexPosts.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const IndexPosts = () => {
+  return (
+    <>
+      <div className="flex justify-center mt-10 mb-10">
+      <div className="flex items-center">
+            <div className="flex border border-purple-200 rounded">
+                <input
+                    type="text"
+                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Search..."
+                />
+                <button className="px-4 text-white bg-purple-600 border-l rounded ">
+                    Search
+                </button>
+            </div>
+        </div>
+
+
+      </div>
+
+
+      
+      <div className="md:flex">
+        <div className="card w-96 bg-base-100 shadow-xl mx-5 mb-10">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card w-96 bg-base-100 shadow-xl mx-5 mb-10">
+          <div className="card-body">
+            <h2 className="card-title">Card title!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default IndexPosts
