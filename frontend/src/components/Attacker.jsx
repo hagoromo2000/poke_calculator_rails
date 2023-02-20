68329579fe5b375cdc23cc9575c89aa3143fafff
hagoromo2000 <revtm000126@gmail.com>
2023-02-21 04:10:51 +0900

    計算機能の雛形を作成 #14

--- frontend/src/components/Attacker.jsx
+++ frontend/src/components/Attacker.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 import './Calculator.css'
 
-const Attacker = () => {
+const Attacker = (props) => {
+
+  const handleAttack = (event) => {
+    props.setAttack(event.target.value)
+  }
+
+  const handlePower = (event) => {
+    props.setPower(event.target.value)
+  }
+
   return (
     <>
       <div className="artboard phone-5 bg-white rounded-lg shadow-xl mx-auto mt-10 ">
@@ -9,7 +19,6 @@ const Attacker = () => {
             <p className="pt-5 pl-5 font-bold " >攻撃側</p>
             <button className="btn brn-primary ml-32 mt-2 mb-2">育成論から呼び出す</button>
           </div>
-          {/* <input htmlFor="pokemon-name-modal" type="text" placeholder="ポケモン名" className="input input-bordered input-secondary w-100 max-w-xs mt-2 ml-2" /> */}
 
           {/* ポケモン選択モーダル　*/}
           <div className="flex mt-5">
@@ -18,16 +27,20 @@ const Attacker = () => {
             <img src="/tera-icon.png" className="mt-auto mb-auto ml-4 w-10 h-10 cursor-pointer" alt="terastal" />
           </div>
           <div className="flex mt-5">
+
+            {/* 攻撃実数値 */}
             <div className="relative">
-              <input type="number" id="attack_floating_filled" className="block rounded-t-lg px-1 pb-2.5 pt-5  ml-4 w-20 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer " placeholder=" " />
+              <input type="number" onChange={handleAttack} id="attack_floating_filled" className="block rounded-t-lg px-1 pb-2.5 pt-5  ml-4 w-20 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer " placeholder=" " />
               <label htmlfor="attack_floating_filled" className="ml-4 absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">実数値</label>
             </div>
 
+            {/* 攻撃努力値 */}
             <div className="relative ml-4">
               <input type="number" id="attack_ev_floating_filled" className="block rounded-t-lg px-1 pb-2.5 pt-5 w-20 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer " placeholder=" " />
               <label htmlfor="attack_ev_floating_filled" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">努力値</label>
             </div>
 
+            {/* 性格補正 */}
             <label className='mt-auto mx-auto mb-auto text-gray-500 '></label>
             <div className="inline-flex rounded-md shadow-sm ml-4 mr-4 " role="group">
               <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
@@ -41,14 +54,16 @@ const Attacker = () => {
               </button>
             </div>
           </div>
-
+          
           <div className="flex mt-5 ml-4">
+            {/* 特性 */}
             <select className="select select-secondary w-40">
               <option disabled selected>特性を選択</option>
               <option>しんりょく</option>
               <option>へんげんじざい</option>
             </select>
 
+            {/* 能力ランク */}
             <p className='mt-auto mb-auto ml-5 text-gray-500 '>ランク 0</p>
 
             <div className="inline-flex rounded-md shadow-smm ml-5" role="group">
@@ -61,6 +76,7 @@ const Attacker = () => {
             </div>
           </div>
 
+          {/* 持ち物 */}
           <div className="flex mt-5 ml-4">
             <select className="select select-secondary w-40">
               <option disabled selected>持ち物を選択</option>
@@ -75,7 +91,7 @@ const Attacker = () => {
             </label>
 
             <div className="relative ml-4">
-              <input type="number" id="attack_ev_floating_filled" className="block rounded-t-lg px-1 pb-2.5 pt-5 w-20 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer " placeholder=" " />
+              <input type="number" onChange={handlePower} id="attack_ev_floating_filled" className="block rounded-t-lg px-1 pb-2.5 pt-5 w-20 text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer " placeholder=" " />
               <label htmlfor="attack_ev_floating_filled" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">威力</label>
             </div>
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = props => {
+  const maxDamage = Math.floor(props.damage)
+  const minDamage = Math.floor(props.damage * 0.85)
+
   return (
     <>
       <footer className="footer pb-4 bg-base-100 text-base-content fixed bottom-0">
@@ -9,10 +12,10 @@ const Footer = () => {
             <p className='ml-10'>計算結果</p>
           </div>
           <div className=" w-full bg-gray-200 rounded-full dark:bg-gray-700 mt-4 mx-4">
-            <div className="bg-blue-600 text-xs font-medium text-blue-100  text-center p-0.5 leading-none rounded-full" style={{width: "45%"}}> 45%</div>
+            <div className="bg-blue-600 text-xs font-medium text-blue-100  text-center p-0.5 leading-none rounded-full" style={{width: `${100-(minDamage / props.hp * 100)}%`}}> {(100 - (minDamage / props.hp * 100)).toFixed(1)}%</div>
           </div>
           <div className='w-full'>
-            <p className='flex justify-end mr-10'>48 ~ 57 (25.6 ~ 30.4%) 確定4発</p>
+            <p className='flex justify-end mr-10'>{minDamage} ~ {maxDamage} ({(minDamage / props.hp * 100).toFixed(1)}% ~ {(maxDamage / props.hp * 100).toFixed(1)}%) 確定4発</p>
           </div>
         </div>
       </footer>
