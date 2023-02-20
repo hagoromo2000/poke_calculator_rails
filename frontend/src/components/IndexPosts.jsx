8c81e699fadd0a2ca6fe49eb13ecbb9ea8e3597d
hagoromo2000 <revtm000126@gmail.com>
2023-02-20 19:59:52 +0900

    ShowPost.jsxの作成 #38

--- frontend/src/components/IndexPosts.jsx
+++ frontend/src/components/IndexPosts.jsx
@@ -9,7 +9,7 @@ const IndexPosts = () => {
                 <input
                     type="text"
                     className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search..."
+                    placeholder="ポケモン名を入力"
                 />
                 <button className="px-4 text-white bg-purple-600 border-l rounded ">
                     Search
