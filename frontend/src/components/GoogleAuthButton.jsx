f65f0d2c9f022717fb274d7203d74fd152aeb6fb
hagoromo2000 <revtm000126@gmail.com>
2023-02-25 11:05:52 +0900

    Add:GoogleログインボタンのCSSを整理

--- frontend/src/components/GoogleAuthButton.jsx
+++ frontend/src/components/GoogleAuthButton.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { auth, provider } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
+import { FcGoogle } from "react-icons/fc";
 
 const GoogleAuthButton = () => {
   return (
     <>
-      <SignInButton />
+      <div className="flex justify-center">
+        <SignInButton />
+      </div>
     </>
   );
 };
@@ -20,8 +23,13 @@ function SignInButton() {
   };
 
   return (
-    <button onClick={signInWithGoogle} className="btn">
-      <p>Googleでサインイン</p>
-    </button>
+    <label
+      htmlFor="signup-modal"
+      onClick={signInWithGoogle}
+      className="bg-white text-gray-600 px-4 py-2 rounded-md flex items-center cursor-pointer"
+    >
+      <FcGoogle className="h-5 w-5 mr-2" />
+      Login with Google
+    </label>
   );
 }
