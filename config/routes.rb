cb1b968a8b49546566a2898b46581dde47c7a1b6
hagoromo2000 <revtm000126@gmail.com>
2023-03-07 17:34:21 +0900

    Add:postsコントローラにmypageアクションを追加 #86

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/auth", to: "authentications#create"
-      resources :posts, only: %i[index show create update destroy]
+      get "/mypage", to: "posts#mypage"
+      resources :posts, only: %i[index create destroy]
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Post.create!(
-  user_id: 1,
-  title: "テスト",
-  body: "テストです",
-  pokemon: "ピカチュウ",
-  tera_type: "でんき",
-  move1: "かみなり",
-  move2: "エレキボール",
-  move3: "アイアンテール",
-  move4: "ボルテッカー",
-  ability: "せいでんき",
-  item: "でんきだま",
-  nature: "いじっぱり",
-  ev_hp: 4,
-  ev_attack: 252,
-  ev_defense: 0,
-  ev_special_attack: 0,
-  ev_special_defense: 0,
-  ev_speed: 252
-)
+10.times do |n|
+  Post.create!(
+    user_id: 1,
+    title: "サンプル#{n}",
+    body: "サンプル#{n}",
+    pokemon: "ピカチュウ",
+    tera_type: "でんき",
+    move1: "かみなり",
+    move2: "エレキボール",
+    move3: "アイアンテール",
+    move4: "ボルテッカー",
+    ability: "せいでんき",
+    item: "でんきだま",
+    nature: "いじっぱり",
+    ev_hp: 4,
+    ev_attack: 252,
+    ev_defense: 0,
+    ev_special_attack: 0,
+    ev_special_defense: 0,
+    ev_speed: 252
+  )
+end
